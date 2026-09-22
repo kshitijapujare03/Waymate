ddffb10bb6b0785bb42b8f82ff9ad05813af875f
samruddhinikharge-hub <samruddhinikharge@gmail.com>
2026-09-23 00:21:29 +0530

    Create ratings.js

---
+++ ratings.js
@@ -0,0 +1,23 @@
+const ratings = [
+    { user: "U1", destination: "Goa", rating: 5 },
+    { user: "U1", destination: "Manali", rating: 3 },
+    { user: "U1", destination: "Jaipur", rating: 4 },
+
+    { user: "U2", destination: "Goa", rating: 4 },
+    { user: "U2", destination: "Kerala Backwaters", rating: 5 },
+    { user: "U2", destination: "Manali", rating: 3 },
+
+    { user: "U3", destination: "Manali", rating: 5 },
+    { user: "U3", destination: "Ladakh", rating: 5 },
+    { user: "U3", destination: "Goa", rating: 3 },
+
+    { user: "U4", destination: "Jaipur", rating: 5 },
+    { user: "U4", destination: "Agra", rating: 4 },
+    { user: "U4", destination: "Hampi", rating: 5 },
+
+    { user: "U5", destination: "Kerala Backwaters", rating: 5 },
+    { user: "U5", destination: "Meghalaya", rating: 5 },
+    { user: "U5", destination: "Goa", rating: 4 }
+];
+
+module.exports = ratings;
